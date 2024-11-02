2d48eb7223e6940d8559376cb4dd51f25bc1e495
anahita.msp.hp@gmail.com <anahita.msp.hp@gmail.com>
2024-11-02 16:35:31 +0330

    creat admin file in shop app

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView , ListView , DeleteView
+from . models import ProductModel,ProductStatusType
 # Create your views here.
-class ShopProductGridView(TemplateView):
+class ShopProductGridView(ListView):
     template_name = "shop/product_grid.html"
+    queryset=ProductModel.objects.filter(status=ProductStatusType.publish.value)
