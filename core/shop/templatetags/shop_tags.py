544b006058adf6a9ea16893673c456bdbc510902
anahita.msp.hp@gmail.com <anahita.msp.hp@gmail.com>
2024-11-04 14:06:20 +0330

    similar products

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -7,3 +7,16 @@ register =template.Library()
 def show_latest_products():
     latest_products=ProductModel.objects.filter(status=ProductStatusType.publish.value).order_by("-created_date")[:8]
     return {"latest_products":latest_products}
+
+@register.inclusion_tag("includes/similar_products.html")
+def show_similar_products(product):
+    product_categories = product.category.all()
+    similar_products = ProductModel.objects.filter(
+        status=ProductStatusType.publish.value,
+        category__in=product_categories
+    ).order_by("-created_date")[:4]
+
+    # Debugging output
+    # print(f"Product: {product.title}, Similar Products Count: {similar_products.count()}")
+
+    return {"similar_products": similar_products}
