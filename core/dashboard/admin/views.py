be5278a1260c7151d8aee9e16fbe8034773d942d
anahita.msp.hp@gmail.com <anahita.msp.hp@gmail.com>
2024-11-21 00:54:59 +0330

    creat security-edit method

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -4,8 +4,15 @@ from django.views.generic import View,TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from accounts.models import UserType
 from dashboard.permissions import HasAdminAccessPermission
-
+from django.contrib.auth import views as auth_views
+from django.contrib.messages.views import SuccessMessageMixin
+from dashboard.admin.forms import AdminPasswordChangeForm
 
 class AdminDashboardHomeView(LoginRequiredMixin,HasAdminAccessPermission,TemplateView):
     template_name="dashboard/admin/home.html"
 
+class AdminSecurityEditView(LoginRequiredMixin,HasAdminAccessPermission,SuccessMessageMixin,auth_views.PasswordChangeView):
+    template_name="dashboard/admin/profile/security-edit.html"
+    form_class=AdminPasswordChangeForm
+    success_url=reverse_lazy("dashboard:admin:security-edit")
+    success_message="بروز رسانی پسوورد با موفقیت انجام شد"
