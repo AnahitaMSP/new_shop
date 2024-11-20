dc1560bc3e42a94a5412f7424991c2282ac5a378
anahita.msp.hp@gmail.com <anahita.msp.hp@gmail.com>
2024-11-20 18:01:52 +0330

    permission for admin and customer

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -3,8 +3,9 @@ from django.urls import reverse_lazy
 from django.views.generic import View,TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from accounts.models import UserType
+from dashboard.permissions import HasAdminAccessPermission
 
 
-class AdminDashboardHomeView(LoginRequiredMixin,TemplateView):
+class AdminDashboardHomeView(LoginRequiredMixin,HasAdminAccessPermission,TemplateView):
     template_name="dashboard/admin/home.html"
 
--- core/dashboard/customer/views.py
+++ core/dashboard/customer/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import View,TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from accounts.models import UserType
+from dashboard.permissions import HasCustomerAccessPermission
 
-
-class CustomerDashboardHomeView(LoginRequiredMixin,TemplateView):
+class CustomerDashboardHomeView(LoginRequiredMixin,HasCustomerAccessPermission,TemplateView):
     template_name="dashboard/customer/home.html"
